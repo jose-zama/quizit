dc51c0485205583c4b876693e0a089d5fa6e5443
Jose <pepeazb@hotmail.com>
2017-06-23 16:01:22 -0700

    Fix for node execution

--- codedeploy/stop.sh
+++ codedeploy/stop.sh
@@ -1,5 +1,4 @@
-#source /home/ec2-user/.bash_profile
-#sudo rm -rf /home/ec2-user/quizit
-which node
+source /home/ec2-user/.bash_profile
+sudo rm -rf /home/ec2-user/quizit
 #sudo ln -s "$(which node)" ~/node-v4.4.7-linux-x64/bin/node
-#forever stopall
+forever stopall
