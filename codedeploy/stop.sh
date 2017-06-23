6cf69994701faa1e190e319632215891e93b78a7
Jose <pepeazb@hotmail.com>
2017-06-23 15:45:41 -0700

    Fix for node execution

--- codedeploy/stop.sh
+++ codedeploy/stop.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+source /home/ec2-user/.bash_profile
 #sudo rm -rf /home/ec2-user/quizit
-sudo ln -s "$(which nodejs)" ~/node-v4.4.7-linux-x64/bin/node
+which node
+sudo ln -s "$(which node)" ~/node-v4.4.7-linux-x64/bin/node
 forever stopall
