ee38f0a1c437ffad33a6279d3c698f7f7f21c97b
Jose <pepeazb@hotmail.com>
2017-06-23 11:17:00 -0700

    Removes bash in codedeploy scripts

--- codedeploy/start.sh
+++ codedeploy/start.sh
@@ -1,3 +1,1 @@
-#!/bin/bash
-
 ~/node-v4.4.7-linux-x64/bin/forever start --workingDir /home/ec2-user/quizit -c "npm start" ./
--- codedeploy/stop.sh
+++ codedeploy/stop.sh
@@ -1,4 +1,2 @@
-#!/bin/bash
-
 sudo rm -rf /home/ec2-user/quizit
 ~/node-v4.4.7-linux-x64/bin/forever stopall
