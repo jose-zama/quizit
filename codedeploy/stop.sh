850c3f4be2f2dedfeba2448123df53404b58687a
Jose <pepeazb@hotmail.com>
2017-06-23 15:30:23 -0700

    Fix for node execution

--- codedeploy/stop.sh
+++ codedeploy/stop.sh
@@ -1,10 +1,3 @@
-PATH=$PATH:$HOME/.local/bin:$HOME/bin
-
-export PATH
-
-export NODE_ENV=production
-
-export PATH="$PATH:/home/ec2-user/node-v4.4.7-linux-x64/bin"
-
-sudo rm -rf /home/ec2-user/quizit
+#!/bin/bash
+#sudo rm -rf /home/ec2-user/quizit
 forever stopall
