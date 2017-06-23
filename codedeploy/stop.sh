c41bfc7af900d72d2d3751e6cb91e0c67b349836
Jose <pepeazb@hotmail.com>
2017-06-23 15:18:29 -0700

    Test updating path

--- codedeploy/stop.sh
+++ codedeploy/stop.sh
@@ -1,3 +1,10 @@
-source /home/ec2-user/.bash_profile
+PATH=$PATH:$HOME/.local/bin:$HOME/bin
+
+export PATH
+
+export NODE_ENV=production
+
+export PATH="$PATH:/home/ec2-user/node-v4.4.7-linux-x64/bin"
+
 sudo rm -rf /home/ec2-user/quizit
 forever stopall
