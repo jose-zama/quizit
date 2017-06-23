4b57a876443e72f4f49e3bce1054468edd994b2e
Jose <pepeazb@hotmail.com>
2017-06-23 11:20:54 -0700

    Set source in codedeploy scripts

--- codedeploy/start.sh
+++ codedeploy/start.sh
@@ -1,1 +1,2 @@
+source /home/ec2-user/.bash_profile
 ~/node-v4.4.7-linux-x64/bin/forever start --workingDir /home/ec2-user/quizit -c "npm start" ./
--- codedeploy/stop.sh
+++ codedeploy/stop.sh
@@ -1,2 +1,3 @@
+source /home/ec2-user/.bash_profile
 sudo rm -rf /home/ec2-user/quizit
 ~/node-v4.4.7-linux-x64/bin/forever stopall
