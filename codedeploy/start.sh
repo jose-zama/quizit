9e3e767c4714a436cf620304485083e90b8a84b0
Jose <pepeazb@hotmail.com>
2017-06-23 18:32:23 -0700

    Fix codeploy start folder

--- codedeploy/start.sh
+++ codedeploy/start.sh
@@ -1,2 +1,3 @@
 source /home/ec2-user/.bash_profile
-~/node-v4.4.7-linux-x64/bin/forever start --workingDir /home/ec2-user/quizit -c "npm start" ./
+cd /home/ec2-user/quizit
+forever start --workingDir /home/ec2-user/quizit -c "npm start" ./
